e5a81272a9881fa546db3e8c637b1bd8b58b3588
Jason Sutter <jasonsutter87@gmail.com>
2026-03-26 15:08:09 -0700

    week1 starting

--- week-01-task-tracker/server/Controllers/TasksController.cs
+++ week-01-task-tracker/server/Controllers/TasksController.cs
@@ -29,46 +29,32 @@ public class TasksController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var tasks = await _db.Tasks.ToArrayAsync();
-        return Ok(tasks);     
+        // var tasks = await _db.Tasks.ToArrayAsync();
+        // return Ok(tasks);     
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
-        var task = await _db.Tasks.FirstOrDefaultAsync(t => t.Id == id);
-        if(task == null) return NotFound();
-        return Ok(task);
+        //TODO: Finish the Controller
     }
 
 
     [HttpPost]
     public async Task<IActionResult> Create(TaskItem task)
     {
-        _db.Tasks.Add(task);
-        await _db.SaveChangesAsync();
-        return Ok(task);
+        //TODO: Finish the Controller
     }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(int id, TaskItem updated)
     {
-        var task = await _db.Tasks.FirstOrDefaultAsync(t => t.Id == id);
-        if(task == null) return NotFound();
-        task.Title = updated.Title;
-        task.Description = updated.Description;
-        task.IsComplete = updated.IsComplete;
-        await _db.SaveChangesAsync();
-        return Ok(task);
+       //TODO: Finish the Controller
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
-        var task = await _db.Tasks.FirstOrDefaultAsync(t => t.Id == id);
-        if(task == null) return NotFound();
-        _db.Remove(task);
-        await _db.SaveChangesAsync();
-        return Ok(task);
+       //TODO: Finish the Controller
     }
 }
